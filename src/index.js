698617224243fb73ec67daf186e287cf21723e7c
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-01-21 00:33:45 +0530

    feat: Add font and logo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './assets/fonts/GalanoGrotesqueMedium.ttf';
+import './assets/fonts/GalanoGrotesqueRegular.ttf';
+
 import App from './components/App';
+import './index.css';
 
 const root = document.getElementById('root');
 
