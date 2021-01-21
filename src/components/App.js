99c890e4c1f65f014f5e456c484c10e51691e8f9
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-01-21 17:21:30 +0530

    feat: Style the top bar for iPad and Desktop

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './Header';
+import DesktopNavbar from './DesktopNavbar';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <DesktopNavbar />
     </div>
   );
 }
---
+++ src/components/DesktopNavbar.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import shopLogo from '../assets/images/shop-logo.jpg';
+import bagIcon from '../assets/images/navicons-bag-inactive.svg';
+import ordersIcon from '../assets/images/navicons-orders-inactive.svg';
+
+import {
+  containerFluid,
+  container,
+  leftSection,
+  rightSection,
+  bagArea,
+  ordersArea,
+  bag,
+  orders,
+  badge,
+  middleSection,
+  input,
+} from '../styles/DesktopNavbar.module.css';
+
+export default function DesktopNavbar() {
+  return (
+    <>
+      <div className={containerFluid}>
+        <div className={container}>
+          <div className={leftSection}>
+            <img src={shopLogo} alt="Logo of Shop" />
+            <h3>Mano Super Market and the name</h3>
+          </div>
+          <div className={rightSection}>
+            <div className={bagArea}>
+              <div className={bag}>
+                <span className={badge}>3</span>
+                <img src={bagIcon} alt="Bag Icon" />
+                <p>Bag</p>
+              </div>
+            </div>
+            <div className={ordersArea}>
+              <div className={orders}>
+                <img src={ordersIcon} alt="Order Icon" />
+                <p>Orders</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={middleSection}>
+        <input
+          type="text"
+          placeholder="Search for products..."
+          className={input}
+        />
+      </div>
+    </>
+  );
+}
