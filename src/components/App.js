b59cabe17ad68d188db401d1376a6f9286a99d01
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-01-24 03:19:03 +0530

    feat: Show different components based on screen width

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,35 @@ import React from 'react';
 import { HashRouter, Route } from 'react-router-dom';
 
 import ShopDesktop from './ShopDesktop';
+import ShopMobile from './ShopMobile';
 import Dashboard from './Dashboard';
 
 export default function App() {
+  const isMobileView = () => {
+    const w = window;
+    const d = document;
+    const documentElement = d.documentElement;
+    const body = documentElement.getElementsByTagName('body')[0];
+    const windowWidth =
+      w.innerWidth || documentElement.clientWidth || body.clientWidth;
+
+    console.log(windowWidth);
+    return windowWidth <= 760;
+  };
+
   return (
     <div>
       <HashRouter>
-        <Route path="/" exact>
-          <ShopDesktop />
-        </Route>
+        <Route
+          path="/"
+          exact
+          render={() => {
+            if (isMobileView()) {
+              return <ShopMobile />;
+            }
+            return <ShopDesktop />;
+          }}
+        ></Route>
         <Route path="/dashboard" exact>
           <Dashboard />
         </Route>
---
+++ src/components/ShopMobile.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ShopMobile() {
+  return <div>Mobile Shop</div>;
+}
