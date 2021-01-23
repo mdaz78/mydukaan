45b705fe0d18dd1f9797afd3ef582aa67bcbb402
Md Abu Zafar <mdabuzafar78@gmail.com>
2021-01-24 00:08:57 +0530

    feat: Style the footer

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from './Header';
 import DesktopNavbar from './DesktopNavbar';
 import ShoppingArea from './ShoppingArea';
+import Footer from './Footer';
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Header />
       <DesktopNavbar />
       <ShoppingArea />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+import freeDelivery from '../assets/images/icons-easyreturns.svg';
+import support from '../assets/images/icons-support.svg';
+import buyerProtection from '../assets/images/icons-protection.svg';
+
+import {
+  containerFluid,
+  container,
+  shopFeatures,
+  storeDetails,
+  storeName,
+  storeAddress,
+} from '../styles/Footer.module.css';
+
+export default function Footer() {
+  return (
+    <div className={containerFluid}>
+      <div className={container}>
+        <section className={shopFeatures}>
+          <section>
+            <img src={freeDelivery} alt="Free delivery" />
+            <p>
+              Free Delivery <span>(above &#8377;500)</span>
+            </p>
+          </section>
+          <section>
+            <img src={buyerProtection} alt="Buyer Protection" />
+            <p>Buyer Protection</p>
+          </section>
+          <section>
+            <img src={support} alt="Customer Support" />
+            <p>Customer Support</p>
+          </section>
+        </section>
+
+        <hr />
+
+        <section className={storeDetails}>
+          <h2>STORE DETAILS</h2>
+          <section>
+            <address>
+              <p className={storeName}>Mano Super Market</p>
+              <p className={storeAddress}>
+                Krishvi Terazzo, 80 Feet Road Indiranagar, Bengaluru Karnataka
+              </p>
+            </address>
+          </section>
+        </section>
+      </div>
+    </div>
+  );
+}
